f8b67da23ec2656279f4d9a8072763f1a54bcd1d
Paweł Stroiński <pol84@live.com>
2018-09-11 23:31:42 +0100

    Update deps

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :url "https://github.com/PawelStroinski/multicall"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
-  :profiles {:dev {:dependencies [[midje "1.9.1"]]}})
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.9.0"]
+                                  [midje "1.9.1"]]}})
